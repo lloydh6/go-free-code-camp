3fbb020aa8b0a0da1f2b6aa59c688b26cb099c3f
lloydh6 <lloyd.hesketh@ans.co.uk>
2023-06-20 15:51:54 +0100

    complex()

--- start.go
+++ start.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var n complex64 = 1 + 2i
-	fmt.Printf("%v, %T\n", real(n), real(n))
-	fmt.Printf("%v, %T\n", imag(n), imag(n))
+	var n complex128 = complex(5, 12)
+	fmt.Printf("%v, %T\n", n, n)
 }
