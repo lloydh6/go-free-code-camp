1c55ccbf29b0d1aa1bef9e520b7d2b5f306a315c
lloydh6 <lloyd.hesketh@ans.co.uk>
2023-06-21 16:43:33 +0100

    more constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	const myConst int = 42
-	fmt.Printf("%v, %T\n", myConst, myConst)
+	const a int = 14
+	const b string = "foo"
+	const c float32 = 3.14
+	const d bool = true
+	fmt.Printf("%v, %T\n", a, a)
+	fmt.Printf("%v, %T\n", b, b)
+	fmt.Printf("%v, %T\n", c, c)
+	fmt.Printf("%v, %T\n", d, d)
 }
