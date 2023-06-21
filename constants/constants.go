43eac2b275ba739306e9542cdb5c591945e506cf
lloydh6 <lloyd.hesketh@ans.co.uk>
2023-06-21 16:45:38 +0100

    using consts

--- constants/constants.go
+++ constants/constants.go
@@ -6,11 +6,6 @@ import (
 
 func main() {
 	const a int = 14
-	const b string = "foo"
-	const c float32 = 3.14
-	const d bool = true
-	fmt.Printf("%v, %T\n", a, a)
-	fmt.Printf("%v, %T\n", b, b)
-	fmt.Printf("%v, %T\n", c, c)
-	fmt.Printf("%v, %T\n", d, d)
+	var b int = 27
+	fmt.Printf("%v, %T\n", a + b, a)
 }
