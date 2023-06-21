70c48c2d509c5d57535629a4189b08049e012d03
lloydh6 <lloyd.hesketh@ans.co.uk>
2023-06-21 16:51:08 +0100

    iota 3

--- constants/constants.go
+++ constants/constants.go
@@ -10,8 +10,13 @@ const (
 	c
 )
 
+const (
+	a2 = iota
+)
+
 func main() {
 	fmt.Printf("%v, %T\n", a, a)
 	fmt.Printf("%v, %T\n", b, b)
 	fmt.Printf("%v, %T\n", c, c)
+	fmt.Printf("%v, %T\n", a2, a2)
 }
